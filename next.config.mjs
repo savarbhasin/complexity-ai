eb87cc4d6ec9f967ccbd6d8c9c3743a98dd085cf
savarbhasin <savarbhasin446@gmail.com>
2025-08-18 01:41:06 +0530

    reduce bundle size

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+import nextBundleAnalyzer from '@next/bundle-analyzer';
+
+const withBundleAnalyzer = nextBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'media.istockphoto.com',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
+};
+
+export default withBundleAnalyzer(nextConfig);
